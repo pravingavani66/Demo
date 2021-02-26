bc9672204794bd3d0f60c589b463041c11a66e81
pravingavani66 <pravingavani66@gmail.com>
2021-02-26 16:25:46 +0530

    ngfor

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,17 +8,85 @@ import { IProduct } from '../models/product.interface';
 })
 export class ProductListComponent implements OnInit {
   title = "product-list";
-  constructor() { }
-  // product: IProduct = {
-  //   productName: 'Hero Honda Splendor',
-  //   description: 'Most popular Indian Bike',
-  //   releaseDate: '10-12-1990',
-  //   price: 200,
-  //   isActive: true,
-  //   imageUrl: 'https://via.placeholder.com/250?text=splendor',
-  // };
-  products: IProduct[] = null;
+  products: IProduct[];
+  constructor() { 
+   this.products
+   =[
+    {
+      productName: 'Hero Honda CD 100',
+      description: 'Most popular Bike of India',
+      releaseDate: '10-08-1990',
+      price: 100,
+      isActive: true,
+      imageUrl: 'https://via.placeholder.com/150?text=CD100SS',
+    },
+    {
+      productName: 'Honda Hornet',
+      description: 'A sports Bike',
+      releaseDate: '10-08-2010',
+      price: 200,
+      isActive: true,
+      imageUrl: 'https://via.placeholder.com/150?text=Hornet',
+    },
+    {
+      productName: 'Super splendor',
+      description: 'A Bike that the nation trusts',
+      releaseDate: '10-08-1980',
+      price: 75,
+      isActive: true,
+      imageUrl: 'https://via.placeholder.com/150?text=Splendor',
+    },
+    {
+      productName: 'Yamaha RX 100',
+      description: 'Nostalgic !',
+      releaseDate: '10-08-1987',
+      price: 122,
+      isActive: false,
+      imageUrl: 'https://via.placeholder.com/150?text=RX100',
+    }];
+  // products: IProduct[] = null;
+}
+refreshData() {
+  console.log('Refreshing !!');
+  this.products =[
+    {
+      productName: 'Hero Honda CD 100',
+      description: 'Most popular Bike of India',
+      releaseDate: '10-08-1990',
+      price: 100,
+      isActive: true,
+      imageUrl: 'https://via.placeholder.com/150?text=CD100SS',
+    },
+    {
+      productName: 'Honda Hornet',
+      description: 'A sports Bike',
+      releaseDate: '10-08-2010',
+      price: 200,
+      isActive: true,
+      imageUrl: 'https://via.placeholder.com/150?text=Hornet',
+    },
+    {
+      productName: 'Super splendor',
+      description: 'A Bike that the nation trusts',
+      releaseDate: '10-08-1980',
+      price: 75,
+      isActive: true,
+      imageUrl: 'https://via.placeholder.com/150?text=Splendor',
+    },
+    {
+      productName: 'Yamaha RX 100',
+      description: 'Nostalgic !',
+      releaseDate: '10-08-1987',
+      price: 122,
+      isActive: false,
+      imageUrl: 'https://via.placeholder.com/150?text=RX100',
+    }];
+  // products: IProduct[] = null;
 
+}
+trackByName(index: number, product: IProduct) {
+  return product.productName;
+}
   ngOnInit(): void {
   }
 
